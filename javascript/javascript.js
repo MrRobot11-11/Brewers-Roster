688d81e348339acfc14ffe5e92a6d0fd10d03564
Riddick <riddick@pop-os.localdomain>
2023-02-22 15:07:02 -0600

    nav bar

---
+++ javascript/javascript.js
@@ -0,0 +1,10 @@
+/* Toggle between adding and removing the "responsive" class 
+to topNav when the user clicks on the icon */
+function toggleTopNav() {
+  var x = document.getElementById("myTopNav");
+  if (x.className === "topNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topNav";
+  }
+} 
