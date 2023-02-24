baf6025af956b0d867f7feed7c3d3dd98e0acefd
Riddick <riddick@pop-os.localdomain>
2023-02-24 15:01:22 -0600

    finished homepage && updated nav bar

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,12 @@
 /* Toggle between adding and removing the "responsive" class 
 to topNav when the user clicks on the icon */
-function toggleTopNav() {
-  var x = document.getElementById("myTopNav");
-  if (x.className === "topNav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topNav";
-  }
-} 
+
+function myFunction() {
+    var x = document.getElementById("topNav");
+    if(x.className === "navBar"){
+        x.className += " responsive";
+    }else {
+        x.className = "navBar";
+    }
+}
 
